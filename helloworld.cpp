ae9f7feb2b9f7dfb8e2272e9c4a05159c747b4e3
eroiyo <eroiyo@outlook.es>
2022-06-13 11:29:27 -0400

    modying task.json

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,11 +1,8 @@
-
 #define GLFW_INCLUDE_NONE
-#include <iostream>
-#include <vector>
-#include <string>
-#include <cstdlib>
+
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
+#include <iostream>
 
 int main() {
   if (!glfwInit()) {
